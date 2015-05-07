7191e37874104b46ce5a055e20449a490092a311
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-05-08 09:43:20 +1000

    move repo path; change mocha reporter

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+'use strict';
+
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
 gulp.task('default', function () {
     return gulp.src('tests/**/*.js', {read: false})
-        .pipe(mocha({reporter: 'nyan'}));
+        .pipe(mocha({reporter: 'spec'}));
 });
